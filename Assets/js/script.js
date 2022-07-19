adb84bcf8c768381c784c63910a828e30db285e8
Saoirse Ford <jacksford78@gmail.com>
2022-07-19 01:34:12 -0700

    finised JS logic and added README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,12 @@
 function init() {
     grabScore()
-}
+};
 
 var startButton = document.querySelector(".start");
 var scoreButton = document.querySelector(".scoreboard");
 var timerPiece = document.querySelector("#time-left");
 var footer = document.querySelector(".footer");
-var quizCont = document.getElementById("quiz-content");
+var quizCont = document.querySelector("#quiz-content");
 
 var score = 0;
 var timeLeft = 0;
@@ -14,7 +14,7 @@ var timer;
 var currentQuestion = -1;
 
 var questions = [{
-    title: "The external JavaScript file must contain the <script> tag.",
+    title: "The external JavaScript file must contain the 'script' tag.",
     choices: ["True", "False"],
     answer: "False"
 },
@@ -49,9 +49,112 @@ startButton.addEventListener("click", gameStart)
 scoreButton.addEventListener("click", grabScore)
 
 function gameStart() {
+    timeLeft = 100;
+    timerPiece.innerHTML = timeLeft;
 
-}
+    timer = setInterval(function() {
+        timeLeft--;
+        timerPiece.innerHTML = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            gameEnd();
+        }
+    }, 1000);
+    loop();
+};
 
 function gameEnd() {
     clearInterval(timer);
-}
+
+    var quizContent = `
+    <p class="p-title-small">Game Over!</p>
+    <p class="p-title-smaller">Your score is ` + score +  ` /120!</p>
+    <p class="p-title-smaller">You got ` + score / 20 +  ` questions correct!</p>
+    <input type="text" id="name" placeholder="Initials here"> 
+    <button class="quiz-button" onclick="setScore()">Save Score!</button>`;
+    
+    quizCont.innerHTML = quizContent;
+    console.log(quizContent)
+};
+
+function goodAnswer() {
+    score += 20;
+    loop();
+};
+
+function badAnswer() {
+    timeLeft -= 10;
+    loop();
+};
+
+function loop() {
+    currentQuestion++;
+      
+    if (currentQuestion > questions.length - 1) {
+        gameEnd();
+        return;
+    }
+
+    var quizContent = `<p class="p-title">` + questions[currentQuestion].title + `</p>`
+  
+    for (var quesOptionLoop = 0; quesOptionLoop < questions[currentQuestion].choices.length; quesOptionLoop++) {
+  
+        var quesButton = `<button class="ques-button" onclick=\"[ANS]\">[CHOICE]</button>`; 
+        quesButton = quesButton.replace("[CHOICE]", questions[currentQuestion].choices[quesOptionLoop]);
+
+        if (questions[currentQuestion].choices[quesOptionLoop] == questions[currentQuestion].answer) {
+            quesButton = quesButton.replace("[ANS]", "goodAnswer()");
+        } else {
+            quesButton = quesButton.replace("[ANS]", "badAnswer()");
+        }
+
+      console.log(quesButton)
+      quizContent += quesButton
+    }
+  
+    console.log(quizContent) 
+    quizCont.innerHTML = quizContent;
+};
+
+function grabScore() {
+    var quizContent = `
+      <p class="p-title">` + localStorage.getItem("highscoreName") + ` your highscore is:</p>
+      <p class="p-title">` + localStorage.getItem("highscore") + `</p><br> 
+      <button class="quiz-button" onclick="clearScore()">Clear score!</button>
+      <button class="quiz-button" onclick="resetGame()">Play Again!</button> `;
+
+      quizCont.innerHTML = quizContent;
+  };
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById("name").value);
+    
+    grabScore()
+
+};
+
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+  
+    timerPiece.innerHTML = timeLeft;
+   
+    var quizContent = `
+    <p id="top-text">Welcome to the Code Knowledge Tester! See just how much you really know!</p>
+    <p id= "bottom-text">Answer questions correctly, and you won't lose time! Answer incorrectly, and your time will decrease, leading to a lower score!</p>
+    <button onclick="gameStart()" class="start">Start!</button>
+    `
+    quizCont.innerHTML = quizContent;
+    console.log(quizContent)
+  }
+
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+  
+    resetGame()
+  };
---
+++ README.md
@@ -0,0 +1,20 @@
+# Code Quiz
+A responsive coding quiz about, and made with Javascript.
+
+## Screenshot
+![image](./Assets/screenshots/screencap.png)
+
+## Technologies Used
+ -HTML - for foundation of the page.
+ - CSS - for style of the page.
+ -Javascript - for logic and functionalityof the page.
+
+## Summary
+When the start button is clicked, the page should begin to question the user about aspects of their Javascript knowledge, and present them with multiple answer buttons to click on. When the user clicks the correct answer, their score should go up by 20 points, and the timer should not have time subtracted. When the user selects an incorrect answer, their score should stay the same, while their timeis subtracted by 15 seconds. After all questions have been answered, the user should be able to save their score and initials to the local storage, and retreive it upon revisiting the page.
+
+## Deployed Link
+*[See live page](https://saintshay.github.io/code-quiz//)
+
+## Creator Links
+[LinkedIn](https://www.linkedin.com/in/jack-ford-a0ab0422a/)
+[GitHub](https://github.com/SaintShay)
